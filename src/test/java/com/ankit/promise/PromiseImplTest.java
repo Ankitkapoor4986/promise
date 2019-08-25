6a4d9b13b470289969548fd874ef5cf7c30a1094
ankitkapoor4986 <ankitkapoor4986@gmail.com>
2019-08-25 21:00:27 +0530

    Tested Javascript function

--- src/test/java/com/ankit/promise/PromiseImplTest.java
+++ src/test/java/com/ankit/promise/PromiseImplTest.java
@@ -15,20 +15,43 @@ public class PromiseImplTest {
     public void shouldRunPromiseToGetSumOfAge(){
 
         List<Person> persons = getPersons();
-        Function<List<Person>, Integer> ageAdderFunction = (personList) -> personList.stream()
-                .mapToInt(Person::getAge).sum();
+        Function<List<Person>, Integer> ageAdderFunction = getAgeAdderFunction();
 
         callPromise(persons, ageAdderFunction);
 
     }
 
+    private Function<List<Person>, Integer> getAgeAdderFunction() {
+        return (people) -> people.stream().mapToInt(Person::getAge).sum();
+    }
+
+    @Test
+    public void shouldRunPromiseWithAcceptAndRejectFunction() {
+        List<Person> persons = getPersons();
+        Function<List<Person>, Integer> ageAdderFunction = getAgeAdderFunction();
+        Promise<Integer> promise = new PromiseImpl<>();
+        Function<List<Person>, Integer> ageSubtract5ThenSum = (people) -> people.stream().mapToInt(Person::getAge)
+                .map(age -> age - 5).sum();
+        Promise<Integer> sumAgeAdderPromise = promise.then(ageAdderFunction, persons, ageSubtract5ThenSum, people -> people.size() < 3);
+        printSum(sumAgeAdderPromise);
+
+
+    }
+
+
+
+
     private void callPromise(List<Person> persons, Function<List<Person>, Integer> ageAdderFunction) {
         Promise<Integer> promise = new PromiseImpl<>();
         Promise<Integer> sumAgeHolderPromise = promise.then(ageAdderFunction, persons);
+        printSum(sumAgeHolderPromise);
+
+
+    }
+
+    private void printSum(Promise<Integer> sumAgeHolderPromise) {
         sumAgeHolderPromise.thenAccept((age) -> System.out.println(age.get()),
                 sumAgeHolderPromise.getValueHolder());
-
-
     }
 
     private List<Person> getPersons() {
@@ -43,17 +66,20 @@ public class PromiseImplTest {
     public void shouldPrintMultipliedSal() {
 
         List<Person> persons = getPersons();
-        Function<List<Person>, List<Person>> multipliedSalFunction = (personsList) -> personsList.stream().
-                peek(person -> person.setSalary(person.getSalary() * 2)).collect(Collectors.toList());
+        Function<List<Person>, List<Person>> multipliedSalFunction = getMulSalaryFunction();
 
         Promise<List<Person>> promise = new PromiseImpl<>();
         Promise<List<Person>> multipliedSalPromise = promise.then(multipliedSalFunction, persons);
         Promise<List<Person>> mulTwicePromise = multipliedSalPromise.then(multipliedSalFunction, multipliedSalPromise.getValueHolder().get());
-        Consumer<List<Person>> consumerPersonList = (personList) -> personList.forEach(person -> System.out.println(person.getSalary()));
+        Consumer<List<Person>> consumerPersonList = (people) -> people.forEach(person -> System.out.println(person.getSalary()));
         mulTwicePromise.thenAccept(consumerPersonList, multipliedSalPromise.getValueHolder().get());
 
     }
 
+    private Function<List<Person>, List<Person>> getMulSalaryFunction() {
+        return (people) -> people.stream().
+                    peek(person -> person.setSalary(person.getSalary() * 2)).collect(Collectors.toList());
+    }
 
 
 }
